30f694d16e591f05489e3a633db1a82ddcd4311b
Evandro2270 <150724259+Evandro2270@users.noreply.github.com>
2024-04-04 16:10:07 -0300

    Adicionar arquivos de projeto.

---
+++ while/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+
+                Console.Write("Digite um numero: ");
+                double x = double.Parse(Console.ReadLine(),CI);
+
+                while (x >= 0.0) {
+                double raiz = Math.Sqrt(x);
+                Console.WriteLine(raiz.ToString("F3" , CI));
+                Console.Write("Digite outro numero: ");
+                x = double.Parse(Console.ReadLine(),CI);    
+                }
+                Console.WriteLine("Numero negativo! ");
+            }
+        }
+    }
+}
